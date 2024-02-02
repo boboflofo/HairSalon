89321d9306464f821b02006df70fd086061e1e7a
Jonathan Song <jonats10@uci.edu>
2024-02-02 13:28:51 -0800

    add create route to client controller

---
+++ HairSalon/Controllers/ClientController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
